fa6f88545d4826333ce4a058c4e51d622846cdd0
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-03 21:17:27 +0100

    Fix merge conflict

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  belongs_to :parent, class_name: 'Comment', optional: true
 
   def update_comment_counter
     post.update(comments_counter: post.comments.count)
