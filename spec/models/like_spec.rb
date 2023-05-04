d25e0dd5045e919af63b0d2da4ce9e938c594309
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-04 19:06:29 +0100

    Add test validation for the like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post, author: author) }
+  let(:subject) { FactoryBot.create(:like, author: author, post: post) }
+
+  describe 'Validations' do
+    it 'requires a user to be present' do
+      expect(subject.author).to be_truthy
+    end
+
+    it 'requires a post to be present' do
+      expect(subject.post).to be_truthy
+    end
+  end
+
+  describe '#update_likes_counter' do
+    it 'updates the likes_counter of the post' do
+      expect(subject.post.likes_counter).to eq(1)
+    end
+  end
 end
