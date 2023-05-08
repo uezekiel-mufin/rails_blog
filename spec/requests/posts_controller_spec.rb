68176c209149ce06a94fd94e8b7fcf26b78f117e
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-08 19:39:52 +0100

    Implemented changes requested by reviewer

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'PostsControllers', type: :request do
   let(:user) { FactoryBot.create(:user) }
-  let(:post) { FactoryBot.create(:post, 3, author: user) }
+  let(:post) { FactoryBot.create(:post, author: user) }
 
-  describe 'GET /users/:user_id/posts' do
+  describe 'GET /index' do
     it 'returns a success response' do
       get "/users/#{user.id}/posts"
       expect(response).to be_successful
@@ -18,6 +18,23 @@ RSpec.describe 'PostsControllers', type: :request do
     it 'response body includes the correct content' do
       get "/users/#{user.id}/posts"
       expect(response.body).to match(%r{<h2>List of posts by a given user</h2>})
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns a success response' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template('show')
+    end
+
+    it 'response body includes the correct content' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to match(%r{<h3>This is the details of a specific post by a given user</h3>})
     end
   end
 end
