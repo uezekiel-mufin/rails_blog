ab6a69282c4ebb283865e1bbb52aadc69bded2ff
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-10 14:18:59 +0100

    Add form for creating new post by a particular user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,33 @@
 class PostsController < ApplicationController
   before_action :set_user, only: %i[index show]
 
+  def show
+    @post = @user.posts.find(params[:id])
+  end
+
   def index
     @posts = @user.posts.paginate(page: params[:page], per_page: 2)
   end
 
-  def show
-    @post = @user.posts.find(params[:id])
+  def new
+
+
   end
+
+  def create
+    title = params[:title]
+    body = params[:body]
+    author = current_user
+    @post = Post.new(title: title, body: body, author: author)
+    if @post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to user_post_path(@post.author, @post)
+    else
+      flash[:alert] = "Post couldn't be created"
+      render 'new'
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users do
-    resources :posts, only: [:index, :show]
+    resources :posts
   end
 end
