1807eac0ec22b27cab20b78bb97819b6df0ed9ac
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-16 23:44:14 +0100

    Add Change password feature

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    if resource.confirmed?
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      flash[:alert] = 'You have to confirm your email address before continuing.'
+      redirect_to new_user_confirmation_path
+    end
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
