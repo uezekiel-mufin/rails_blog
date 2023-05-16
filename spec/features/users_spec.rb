893d369115016afbc7842c56a4ea1be9f0fba76f
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-16 09:05:39 +0100

    Add capybara test for users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -9,11 +9,76 @@ RSpec.feature "Users", type: :feature do
     FactoryBot.create(:post, author: @user2)
     FactoryBot.create(:post, author: @user3)
   end
+  describe 'Users Index' do
 
-  scenario 'I can see the usernames of all other users' do
-    visit root_path
-    expect(page).to have_content(@user1.name)
-    expect(page).to have_content(@user2.name)
-    expect(page).to have_content(@user3.name)
+    scenario 'I can see the usernames of all other users' do
+      visit root_path
+      expect(page).to have_content(@user1.name)
+      expect(page).to have_content(@user2.name)
+      expect(page).to have_content(@user3.name)
+    end
+
+    scenario 'I can see the profile picture of all other users' do
+      visit root_path
+      expect(page).to have_selector("img[src='https://i.pravatar.cc/300?img=1']")
+      expect(page).to have_selector("img[src='https://i.pravatar.cc/300?img=2']")
+      expect(page).to have_selector("img[src='https://i.pravatar.cc/300?img=3']")
+    end
+
+    scenario 'I can see the number of posts of all other users' do
+      visit root_path
+      expect(page).to have_content(@user1.posts_counter)
+      expect(page).to have_content(@user2.posts_counter)
+      expect(page).to have_content(@user3.posts_counter)
+    end
+
+    scenario 'When i click on a username, I am taken to that user\'s profile page' do
+      visit root_path
+      click_link @user1.name
+      sleep(5)
+      expect(current_path).to eq(user_path(@user1.id))
+    end
+  end
+
+  describe 'User show page' do
+    scenario 'I can see the user profile picture' do
+      visit user_path(@user1.id)
+      expect(page).to have_selector("img[src='https://i.pravatar.cc/300?img=1']")
+    end
+
+    scenario 'I can see the user name' do
+      visit user_path(@user1.id)
+      expect(page).to have_content(@user1.name)
+    end
+
+    scenario 'I can see the number of posts of the user' do
+      visit user_path(@user1.id)
+      expect page.has_content?(@user1.posts_counter)
+    end
+
+    scenario 'I can see the user bio' do
+      visit user_path(@user3.id)
+      expect(page).to have_content(@user3.bio)
+    end
+
+    scenario 'I can see a button that lets me view all of the user\'s posts' do
+      visit user_path(@user3.id)
+      expect page.has_link?('See all Posts')
+    end
+
+    scenario 'When I click on the button, I am redirected to the user\'s posts index page' do
+      visit user_path(@user3)
+      within '.btn_container' do
+        click_button 'See all Posts'
+      end
+      expect(current_path).to eq(user_posts_path(@user3))
+    end
+
+    scenario 'When I click on a post, I am taken to that post\'s show page' do
+      visit user_posts_path(@user3, @user3.posts)
+      click_link @user3.posts.first.title
+      expect(current_path).to eq(user_post_path(@user3, @user3.posts.first ))
+    end
+
   end
 end
