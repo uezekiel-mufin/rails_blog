e25185275ee517460c2d3289f07fb5279bef51c9
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-16 16:34:21 +0100

    Setup devise

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'faker', '~> 2.18', '>= 2.18.0'
 
+# use the devise gem for authentication
+gem 'devise'
+
 gem 'will_paginate', '~> 3.3'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_141349) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_150731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,18 +44,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_141349) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", primary_key: "author_id", id: :bigint, default: -> { "nextval('users_id_seq'::regclass)" }, force: :cascade do |t|
     t.string "name"
     t.text "photo"
     t.string "bio"
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "email", default: "", null: false
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users", column: "author_id", primary_key: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "author_id", primary_key: "author_id"
+  add_foreign_key "posts", "users", column: "author_id", primary_key: "author_id"
 end
