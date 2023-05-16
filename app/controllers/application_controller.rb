00b157527e4c3d7592a8c10c99b5e2da63063ab0
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-16 16:52:55 +0100

    Ad user sign up feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :bio, :photo)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :bio, :photo, :password, :current_password)}
+  end
+
   def current_user
     User.first
   end
