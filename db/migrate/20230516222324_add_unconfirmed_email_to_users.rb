8da89e32c4d339ac3ab77c54f170d24c5ab48c8f
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-17 17:24:23 +0100

    Implemented changes requested by the reviewer

--- db/migrate/20230516222324_add_unconfirmed_email_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUnconfirmedEmailToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :unconfirmed_email, :string
-    add_index :users, :unconfirmed_email, unique: true
-  end
-end
