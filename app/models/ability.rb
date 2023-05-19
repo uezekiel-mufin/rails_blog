fe0898f3b304692ccb4096f7a7716f8ac6ea974d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-19 08:51:03 +0100

    Remove commented code in ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # # Define abilities for the user here. For example:
-    #   return unless user.present?
-    #   can :read, :all
-    #   can :manage, Post, author: user
-    #   can :manage, Comment, author: user
-    #   return unless user.admin?
-    #   can :manage, :all
-    # end
 
     can :read, [Post, Comment]
     return unless user.present?
