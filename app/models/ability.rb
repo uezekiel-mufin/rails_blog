1f9c269a972070caed436022e1b491a7fc53a059
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-19 08:56:23 +0100

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     can :read, [Post, Comment]
     return unless user.present?
-
     can :manage, [Post, Comment], author_id: user.id
     return unless user.admin?
-
     can :manage, :all
   end
 end
