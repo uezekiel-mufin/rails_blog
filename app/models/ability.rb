10621731b6a247f234965d003ed52e6f6747e282
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-19 08:58:44 +0100

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,10 @@ class Ability
   def initialize(user)
     can :read, [Post, Comment]
     return unless user.present?
+
     can :manage, [Post, Comment], author_id: user.id
     return unless user.admin?
+
     can :manage, :all
   end
 end
