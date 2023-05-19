f0bc130e47c7ed0bdbd08d3aa128a5d76f7b5d4f
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-19 08:42:16 +0100

    Add ability to user to access resource

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,10 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
       return unless user.present?
-
       can :read, :all
       can :manage, Post, author: user
       can :manage, Comment, author: user
       return unless user.admin?
-
       can :manage, :all
     end
 
